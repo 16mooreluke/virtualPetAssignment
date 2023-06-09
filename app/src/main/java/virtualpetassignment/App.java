9a3399439238e90ae2fef5bb3e5e815c46156ccb
16mooreluke <you@example.com>
2023-06-09 11:36:27 -0400

    1:1 push

--- app/src/main/java/virtualpetassignment/App.java
+++ app/src/main/java/virtualpetassignment/App.java
@@ -9,29 +9,62 @@ public class App {
     public static void main(String[] args) {
     
         Scanner input = new Scanner(System.in);
-        VirtualPet petStats = new VirtualPet();
+        VirtualPet userPet = new VirtualPet();
         Boolean keepGoing = true;
 
         System.out.println("Welcome to Luke's Animal Sanctuary!");
         System.out.println("At our establishment, you get to be in charge of one of our lovely and adorable pets!");
         System.out.println("You will be responsible for keeping the pet fed and watered, entertained, and rested.");
         System.out.println("Each time you start an activity, it will take an amount of turns listed next to the action.");
-        System.out.println("Make sure to maintain the stats of the pet! If any reach zero it's game over!");
+        System.out.println("Make sure to maintain the stats of the pet! If any stat reaches zero it's game over!");
         System.out.println("Let's get started, shall we?");
         System.out.println("What is the name of your pet?");
         String petName = input.nextLine();
 
         do {
-            System.out.println(petName + "stats");
-            System.out.println("Food/Water: " + nutrients);
-            System.out.println("Play: " + play);
-            System.out.println("Rest: " + rest);
+            System.out.println(petName + " stats");
+            System.out.println("Food: " + userPet.getFood());
+            System.out.println("Hydration: " + userPet.getHydration());
+            System.out.println("Play: " + userPet.getPlay());
+            System.out.println("Rest: " + userPet.getRest());
             
             System.out.println("What would you like to do?");
-            System.out.println("1. Feed/water" + petName);
-            System.out.println("2. Play with" + petName);
-            System.out.println("3. Let " + petName + " rest");
+            System.out.println("A. Feed " + petName);
+            System.out.println("B. Hydrate " + petName);
+            System.out.println("C. Play with " + petName);
+            System.out.println("D. Let " + petName + " rest");
+            System.out.println("E. Exit the game.");
+            String userInput = input.nextLine();
+            userInput = userInput.trim().toLowerCase();
 
+
+            if(userInput.equals("a")) {
+                System.out.println("You have fed " + petName + ".");
+                userPet.feed();
+            }
+            if(userInput.equals("b")) {
+                System.out.println("You have watered " + petName + ".");
+                userPet.water();
+            }
+            if(userInput.equals("c")) {
+                System.out.println("You have played with " + petName + ".");
+                userPet.play();
+            }
+            if(userInput.equals("d")) {
+                System.out.println("You have let " + petName + " rest.");
+                userPet.rest();
+            }
+            if(userInput.equals("e")) {
+                break;
+            }
+            userPet.tick();
+
+            if (!userPet.isItOkay()) {
+                System.out.println("Oh no! Looks like " + petName + " isn't feeling great.");
+            }
+            if (!userPet.isItDead()) {
+                break;
+            }
 
 
         } while (keepGoing);
--- app/src/main/java/virtualpetassignment/VirtualPet.java
+++ app/src/main/java/virtualpetassignment/VirtualPet.java
@@ -1,11 +1,79 @@
 package virtualpetassignment;
 
 public class VirtualPet {
-    private int nutrients;
+    private int food = 9;
+    private int hydration = 8;
     private int play = 5;
     private int rest = 7;
 
-    public void tick() {
-        for (int i = 0, i <= )
+    public int getFood() {
+        return food;
     }
+    public int getHydration() {
+        return hydration;
+    }
+    public int getPlay() {
+        return play;
+    }
+    public int getRest() {
+        return rest;
+    }
+
+    public void tick() {
+        food -= 1;
+        hydration -= 1;
+        play -=  1;
+        rest -= 1;
+    }
+
+    public void feed() {
+        food += 4;
+    }
+    public void water() {
+        hydration += 4;
+    }
+    public void play() {
+        play += 5;
+        food -= 2;
+        hydration -= 3;
+        rest -= 3;
+    }
+    public void rest() {
+        rest += 6;
+        play -= 4;
+    }
+
+    public boolean isItOkay() {
+        Boolean petHealth = true;
+        if (food <= 3) {
+            petHealth = false;
+        }
+        if (hydration <= 3) {
+            petHealth = false;
+        }
+        if (play <= 3) {
+            petHealth = false;
+        }
+        if (rest <= 3) {
+            petHealth = false;
+        }
+        return petHealth;
+    }
+    public boolean isItDead() {
+        Boolean petHealth = true;
+        if (food == 0) {
+            petHealth = false;
+        }
+        if (hydration == 0) {
+            petHealth = false;
+        }
+        if (play == 0) {
+            petHealth = false;
+        }
+        if (rest == 0) {
+            petHealth = false;
+        }
+        return petHealth;
+    }
+
 }
